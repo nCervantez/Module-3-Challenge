dad1f447054fe54aa2ccddf0d272a2a912b06778
nCervantez <nathaniel.cervantez@yahoo.com>
2023-07-02 18:03:58 -0500

    Successfully added functions to print total number of voters, and added each candidate to a list.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,10 +5,36 @@ import csv
 #setting the path for the file
 election_path = os.path.join("Resources", "election_data.csv")
 
+print("Election Results")
+print("--------------------------")
+
+#varable to store the total number of voters
+total=0
+
+
 #Will open the csv file 
 with open(election_path) as csv_file:
     csvreader = csv.reader(csv_file, delimiter=",")
 
     #Skips the header in the csv file
     csv_header = next(csv_file)
+    #prints the total number of rows in the csv file
+    total = len(list(csvreader))
+    print(f"Total Votes:  {total}")
+
+print("--------------------------")
+
+#list to hold the names on the candidates
+candidates = []
+
+#will go through the csv file and store the unique names if all candidates
+with open(election_path) as csv_file:
+    csvreader = csv.reader(csv_file, delimiter=",")
+
+    #Skips the header in the csv file
+    csv_header = next(csv_file)
+
+    for row in csvreader:
+        if row[2] not in candidates:
+            candidates.append(row[2])
 
