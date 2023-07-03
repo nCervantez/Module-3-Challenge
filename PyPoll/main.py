a9c53459104714a7e689c6dfde8d68a4deb101df
nCervantez <nathaniel.cervantez@yahoo.com>
2023-07-03 02:50:52 -0500

    Analysis file generated correctly. Added the final lines of code taht will write what is printed on the terminal. It will write every candidate and how many votes and percentage of votes the candidates receive respectively.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -58,7 +58,7 @@ with open(election_path) as csv_file:
             if str(row[2]) == candidate:
                 votes[candidate] += 1
 #this will convert the votes dict to a dict that has lists for values so that i 
-#may add teh percentages associated with each candidate
+#may add the percentages associated with each candidate
 new_votes = {key: [value] for key, value in votes.items()}
 
 #this will calculate the percentage vote for each candidate
@@ -71,7 +71,10 @@ for value in votes.values():
 for key, value in new_votes.items():
     value.append(percentages.pop(0))
 
-print(new_votes)
+#This will print the candidates name, the percentage of votes, and total number of votes in
+#a single line for each item in a dictionary.
+for key, value in new_votes.items():
+    print(f"{key}: {round(value[1],3)}% ({value[0]})")
 
 
 winner = max(votes, key=votes.get)
@@ -79,3 +82,32 @@ winner = max(votes, key=votes.get)
 print("--------------------------")
 print(f"Winner: {winner}")
 print("--------------------------")
+
+
+
+#Path for the output file.
+output_path = os.path.join("analysis", "voting_analysis.txt")
+
+#this will output all the given data to a text file.
+with open(output_path, "w",) as datafile:
+    
+    #This will write the beginning of the information needed.
+    initial_write = ["Election Results", "---------------------", "Total Votes: " + str(total),
+                     "--------------------"]
+    
+    for line in initial_write:
+        datafile.write(line + '\n')
+    
+    #List to be written to the text file. str() functions to cast my variables 
+    #as strings and write them into the text file. seperate for loops to write each value in the dict
+    for key, value in new_votes.items():
+        data = [str(key)+ ": " + str(round(value[1],3)) + "% (" + str(value[0]) + ")"
+                ]
+        for line in data:
+            datafile.write(line + '\n')
+
+    #last write data that is needed after the dict is written
+    end_write = ["-------------------", "Winner: " + str(winner), "--------------------"]
+
+    for line in end_write:
+        datafile.write(line + '\n')
